db05bf66a5e94607032fc3acfc589c9186031622
unknown <joshuameier00@gmail.com>
2018-12-04 20:00:39 -0600

    first level design

---
+++ Assets/playermovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	public float runspeed = 40f;
+	
+	float horizontalMove = 0f;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+		
+	}
+
+	void FixedUpdate () {
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+	}
+}
